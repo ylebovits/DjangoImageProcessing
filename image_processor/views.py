bb3db4f0251de8985dabc3b6b622d63d0b21f377
Yakir <yakirlebovits@temple.edu>
2020-03-16 02:26:35 -0400

    migrated static files and templates

--- image_processor/views.py
+++ image_processor/views.py
@@ -1,7 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-
 def index(request):
+    context = {"title": "Home", "image": None}
+
+    if request.method == "POST":
+        context["image"] = "/img/sample.bmp"
+        for k,v in request.POST.items():
+            print(k, v)
+
     return render(request=request,
-                  template_name="index.html",
-                  context={"title": "Home"})
+              template_name="index.html",
+              context=context)
--- main/settings.py
+++ main/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'main/../image_processor/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -119,4 +119,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/main/templates/static/'
