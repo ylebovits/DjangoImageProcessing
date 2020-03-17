be8d830ac385a158e6a67f251c62a938330f0d73
Yakir <yakirlebovits@temple.edu>
2020-03-17 03:53:40 -0400

    created filter functionality

--- image_processor/choices.py
+++ image_processor/choices.py
@@ -1,9 +1,9 @@
 FILTER_CHOICES = (
-    (1, "Solar"),
-    (2, "Edge"),
-    (3, "Poster"),
-    (4, "Blur"),
-    (5, "Gray"),
-    (6, "Sepia")
+    ("solar", "Solar"),
+    ("edge", "Edge"),
+    ("poster", "Poster"),
+    ("blur", "Blur"),
+    ("gray", "Gray"),
+    # ("sepia", "Sepia")
 )
 
--- image_processor/views.py
+++ image_processor/views.py
@@ -1,7 +1,12 @@
+import os
+from django.conf import settings
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
 from django.shortcuts import render
 from image_processor.forms import UploadForm
+from PIL import Image, ImageOps, ImageFilter
 
-
+# all steps of processing (upload, filter, display) are done on the home page
 def index(request):
     context = {"title": "Home", "form": UploadForm,
                "image": None, "errors": None}
@@ -10,13 +15,60 @@ def index(request):
         upload = UploadForm(request.POST, request.FILES)
 
         if upload.is_valid():
-            context["image"] = "/img/sample.bmp"
-            print("good!")
+            file = upload.cleaned_data["imgFile"]
+            tmp_imgPath = default_storage.save("tmp.bmp", ContentFile(file.read()))
+            tmp_imgPath = os.path.join(settings.MEDIA_URL, tmp_imgPath)
+            imgPath = process_image(tmp_imgPath, upload.cleaned_data["filter"])
+            context["image"] = imgPath
 
         else:
             context["errors"] = upload.errors
-            print(context["errors"])
 
     return render(request=request,
                   template_name="index.html",
                   context=context)
+
+
+# use PIL to apply filters to image
+def process_image(file_name: str, img_filter: str):
+    img = Image.open(file_name)
+
+    # some issue here that I don't want to fix
+    # if img_filter == "sepia":
+    #     sepia = []
+    #     r, g, b = (239, 224, 185)
+    #     for i in range(255):
+    #         sepia.extend((r * i / 255, g * i / 255, b * i / 255))
+    #     img = img.convert("L")
+    #     img.putpalette(sepia)
+    #     img = img.convert("RGB")
+
+    if img_filter == "blur":
+        img = img.filter(ImageFilter.BLUR)
+
+    if img_filter == "edge":
+        img = ImageOps.grayscale(img)
+        img = img.filter(ImageFilter.FIND_EDGES)
+
+    if img_filter == "solar":
+        img = ImageOps.solarize(img, threshold=80)
+
+    if img_filter == "gray":
+        img = ImageOps.grayscale(img)
+
+    if img_filter == "poster":
+        img = ImageOps.posterize(img, 3)
+
+    out_file_name = "-out.".join(file_name.split("."))
+
+    img.save(out_file_name)
+    return out_file_name
+
+
+
+
+
+
+
+
+
