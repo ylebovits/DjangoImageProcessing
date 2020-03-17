975b9dc4a3bdd03b1cacc52ba04508e96f8fad38
Yakir <yakirlebovits@temple.edu>
2020-03-17 02:20:56 -0400

    fixed file uploading

--- image_processor/views.py
+++ image_processor/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from image_processor.forms import UploadForm
 
+
 def index(request):
-    context = {"title": "Home", "image": None}
+    context = {"title": "Home", "form": UploadForm, "image": None}
 
     if request.method == "POST":
-        form = UploadForm(request.POST)
-        print(form.filter)
-        if form.is_valid():
+        upload = UploadForm(request.POST, request.FILES)
+
+        if upload.is_valid():
             context["image"] = "/img/sample.bmp"
             print("good!")
         else:
-            print("bad form!")
+            print(upload.errors)
 
     return render(request=request,
                   template_name="index.html",
