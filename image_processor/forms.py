2a42f1e108f07c133e653bc99a9cf3d1e864038c
Yakir <yakirlebovits@temple.edu>
2020-03-17 02:21:29 -0400

    specified data field parameters

--- image_processor/forms.py
+++ image_processor/forms.py
@@ -3,6 +3,7 @@ from image_processor.choices import FILTER_CHOICES
 
 
 class UploadForm(forms.Form):
-    imgFile = forms.FileField()
-    filter = forms.ChoiceField(choices=FILTER_CHOICES)
+    imgFile = forms.FileField(required=True)
+    filter = forms.ChoiceField(choices=FILTER_CHOICES,
+                               required=True)
 
