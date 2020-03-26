2e1517f05da512bc2e842ad6dc2d6a9529d899fb
Ubuntu <ubuntu@ip-172-31-45-189.ec2.internal>
2020-03-26 12:59:51 +0000

    updates to s3 connection

--- image_processor/views.py
+++ image_processor/views.py
@@ -18,14 +18,19 @@ def index(request):
 
         if upload.is_valid():
             file = upload.cleaned_data["imgFile"]
-            ext = file.name.split(".")[1]
+            name, ext = file.name.split(".")
 
             # TODO:
             # modify this to read/write S3 rather than local disk
 
-            tmp_imgPath = default_storage.save(f'tmp.{ext}', ContentFile(file.read()))
+            tmp_imgPath = default_storage.save(f'{name}.{ext}', ContentFile(file.read()))
             tmp_imgPath = os.path.join(settings.MEDIA_URL, tmp_imgPath)
             imgPath = process_image(tmp_imgPath, upload.cleaned_data["filter"])
+
+            if not imgPath:
+                imgPath = None
+                context["errors"] = {"error": ["Error processing image"]}
+
             context["image"] = imgPath
 
         else:
@@ -36,53 +41,66 @@ def index(request):
                   context=context)
 
 
-# use PIL to apply filters to image
+# use PIL to apply 1 or 5 filers to image
 def process_image(file_name: str, img_filter: str):
     img = Image.open(file_name)
 
-    # some issue here that I don't want to fix
-    # if img_filter == "sepia":
-    #     sepia = []
-    #     r, g, b = (239, 224, 185)
-    #     for i in range(255):
-    #         sepia.extend((r * i / 255, g * i / 255, b * i / 255))
-    #     img = img.convert("L")
-    #     img.putpalette(sepia)
-    #     img = img.convert("RGB")
+    try:
+        if img_filter == "blur":
+            img = img.filter(ImageFilter.BLUR)
 
-    if img_filter == "blur":
-        img = img.filter(ImageFilter.BLUR)
+        if img_filter == "edge":
+             img = ImageOps.grayscale(img)
+             img = img.filter(ImageFilter.FIND_EDGES)
 
-    if img_filter == "edge":
-        img = ImageOps.grayscale(img)
-        img = img.filter(ImageFilter.FIND_EDGES)
+        if img_filter == "solar":
+             img = ImageOps.solarize(img, threshold=80)
+    
+        if img_filter == "gray":
+             img = ImageOps.grayscale(img)
 
-    if img_filter == "solar":
-        img = ImageOps.solarize(img, threshold=80)
+        if img_filter == "poster":
+             img = ImageOps.posterize(img, 3)
 
-    if img_filter == "gray":
-        img = ImageOps.grayscale(img)
-
-    if img_filter == "poster":
-        img = ImageOps.posterize(img, 3)
+    except Exception as e:
+        return False
 
     out_file_name = "-out.".join(file_name.split("."))
 
     img.save(out_file_name)
-    s3_upload(out_file_name)
-    return out_file_name
+
+    os.remove(file_name)  # remove original image copy once it has been processed
+
+    if not s3_upload(out_file_name):
+        print("error uploading image")
+
+    return s3_download(out_file_name)
 
 
 def s3_upload(file_name: str):
-    s3 = boto3.resource("s3")
+     s3 = boto3.resource("s3")
 
+     try:
+         with open(file_name, "rb") as file_content:
+             s3.Bucket("ylcis4517project1").put_object(Key=file_name, Body=file_content)
+
+         os.remove(file_name)  # once successfully uploaded, remove local copy
+         return True
+
+     except Exception as e:
+        print(e)
+        return False
+
+
+def s3_download(key: str):
     try:
-        with open(file_name, "wb") as file_content:
-            s3.Bucket("ylcis4517project1").put_object(Key=file_name,
-                                                      Body=file_content)
-        return "success"
+        s3 = boto3.resource("s3")
+
+        tmp_name = "-s3.".join(key.split("."))
+        s3.Object("ylcis4517project1", key).download_file(tmp_name)
+        return tmp_name
+
     except Exception as e:
-        return e
-
-
+        print(e)
+        return False
 
